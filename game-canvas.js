d78d9f54c7caef6e4181d2f582d8fd98def0d443
zayle <email@zayle.me>
2016-11-03 12:20:05 -0600

    Game Canvas

--- game-canvas.js
+++ game-canvas.js
@@ -2,15 +2,10 @@ class GameCanvas {
     constructor(canvas) {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
-        this.Resize16x9();
     }
 
     DrawBorders() {
-        var x = 0;
-        var y = 0;
-        var w = this.canvas.width;
-        var h = this.canvas.height;
-        this.ctx.strokeRect(x, y, w, h);
+        this.ctx.strokeRect(0, 0, this.canvas.width, this.canvas.height);
     }
 
     FillBackground(color) {
